1964f956da6e63e2088c26ea312467562d56a315
Lucca Tirelli <luccatirelli@hotmail.com>
2024-04-02 14:16:26 -0300

    First commit

---
+++ src/main/java/me/dio/sdw24/application/AskChampionUseCase.java
@@ -0,0 +1,28 @@
+package me.dio.sdw24.application;
+
+import me.dio.sdw24.domain.exception.ChampionNotFoundException;
+import me.dio.sdw24.domain.model.Champion;
+import me.dio.sdw24.domain.ports.ChampionsRepository;
+import me.dio.sdw24.domain.ports.GenerativeAiService;
+
+public record AskChampionUseCase(ChampionsRepository repository, GenerativeAiService genAiService) {
+
+    public String askChampion(Long championId, String question){
+
+        Champion champion = repository.findById(championId)
+                .orElseThrow(() -> new ChampionNotFoundException(championId));
+
+        String context = champion.generateContextByQuestion(question);
+        String objective = """
+                Atue como uma assistente com a habilidade de se comportar como os campeões do League of Legends (LoL).
+                Responda perguntas  incorporando a personalidade e estilo de um determinado campeão.
+                Segue a pergunta, o nome do campeão e sua respectiva lore (história):
+                
+                """;
+
+
+
+        return genAiService.generateContent(objective, context);
+    }
+
+}
